7343fbdc25cb0cf348c9d3bacd6d2079fe060922
Balbader <15971200+Balbader@users.noreply.github.com>
2021-01-14 14:24:19 +0100

    update

--- lib/00_journalists.rb
+++ lib/00_journalists.rb
@@ -1,21 +1,52 @@
-def elements_number
-  arr = ["1", "12", "123", "1234", "12345", "123456", "1234567", "12345678"]
-  puts "This arr has a total of " +  arr.length.to_s + "handles."
+arr = ["aS", "sA2", "JS123", "s234", "s2345"]
+
+def elements_number(arr)
+  puts "This arr has a total of " +  arr.length.to_s + " handles."
 end
 
-def elements_length
-  arr = ["1", "12", "123", "1234", "12345", "123456", "1234567", "12345678"]
-  i = 0
-  while arr[i].length
-    puts "array element length: " + arr[i].length.to_s
-    i += 1
+def elements_short(arr)
+  puts "The smallest handle in this array has #{arr.min_by {|x| x.length}} character."
+end
+
+def five(arr)
+  puts "There are a total of #{arr.count {|x| x.length == 5}} handles with 5 characters."
+end 
+
+def maj(arr)
+  def is_upper?
+    self == self.upcase
   end
+  i = 0
+  count = 0
+  while arr.length
+    up = arr.map {|x| x.to_s}[i][1].chars.first.is_upper? 
+    if up == true
+      i += 1
+      count += 1
+      puts count
+    else
+    i += 1
+    end
+  end
+  print count
 end
 
-def five_char
-
+def reorder_alpha(arr)
+  puts arr.sort
 end
 
-elements_number
-elements_length
-five_char
+def handle_size
+  
+end
+
+
+
+
+
+=begin
+elements_number(arr)
+elements_short(arr)
+five(arr)
+maj(arr)
+reorder_alpha(arr)
+=end
